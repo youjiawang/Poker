1b148a7cdac450428ee12d13f9be5ac3d282780b
metoo5566 <351757417@qq.com>
2017-06-19 09:11:49 +0800

    33333

--- assets/code/cow/cow.js
+++ assets/code/cow/cow.js
@@ -2,20 +2,16 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        poker: cc.SpriteAtlas,
+        pokerSmall: cc.SpriteAtlas,
+
+        btReady: cc.Node,
+        spineNode: cc.Node,
+        pokeNode: [cc.Node]
     },
 
     // use this for initialization
-    onLoad: function () {
+    onLoad: function() {
 
     },
 
@@ -23,4 +19,27 @@ cc.Class({
     // update: function (dt) {
 
     // },
+    showCard(data) {
+        for (let k in data) {
+            let card = new cc.Node();
+            card.addComponent(cc.Sprite).spriteFrame = this.poker.getSpriteFrame('pkc_' + data[k].value + '0' + (data[k].color + 1));
+            card.setScale(0.8);
+            card.parent = cc.find('Canvas/My');
+        }
+    },
+    showStart() {
+        this.btReady.active = false;
+        let snode = cc.find('start', this.spineNode);
+        snode.active = true;
+        snode.getComponent('sp.Skeleton').setAnimation(0, 'start', false);
+        snode.getComponent('sp.Skeleton').setCompleteListener(() => {
+            snode.active = false;
+            let data = [{ color: 0, value: 1 },
+                { color: 0, value: 3 },
+                { color: 0, value: 6 },
+                { color: 0, value: 9 },
+            ]
+            this.showCard(data);
+        })
+    },
 });
